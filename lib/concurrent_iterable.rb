1cf807b91fcf4cbdd3a3d03792aef5433133d3ae
Tom Collier <collier@apartmentlist.com>
2019-11-14 22:23:38 -0800

    Add Iterator#each

--- lib/concurrent_iterable.rb
+++ lib/concurrent_iterable.rb
@@ -1,5 +1,15 @@
+require 'concurrent_iterable/config'
+require 'concurrent_iterable/iterator'
 require 'concurrent_iterable/version'
 
 module ConcurrentIterable
-  class Error < StandardError; end
+  class << self
+    def configure
+      yield config
+    end
+
+    def config
+      @config ||= Config.new
+    end
+  end
 end
